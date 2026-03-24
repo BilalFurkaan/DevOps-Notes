5e181c45f056e8355c973771123e5bc03673ee3c
BilalFurkaan <bfurkankaraca1@gmail.com>
2026-03-24 01:58:22 +0000

    Bash script temeller eklendi

---
+++ bash-temeller.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Degisken tanimlama
+
+isim="Furkan"
+echo "Merhaba $isim!"
+
+#Tarih bilgisi
+tarih=$(date)
+echo "Bugunun tarihi: $tarih"
+
+# Koşul tanımlama
+sayi=10
+
+if [ $sayi -gt 5 ]; then 
+    echo "$sayi sayısı 5 den büyük"
+else
+    echo "$sayi sayısı 5 den küçük"
+fi 
+
+# Döngüler
+for i in 1 2 3 4 5 ; do 
+    echo "Sayi: $i"
+done
+
+# Klasördeki dosyaları döngüyle listele
+
+for dosya in /home/furkan/devops/*; do
+    echo "Dosya bulundu: $dosya"
+done
